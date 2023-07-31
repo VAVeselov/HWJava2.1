5c22b6935b1d6ad3ebd9eaeeadaaf26685075b21
Влад Веселов <veselo.vlad9976@gmail.com>
2023-07-31 21:41:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15000;
+        int x = 20;
+        int i = amount / x;
+        System.out.println(i);
+
+
+    }
+}
